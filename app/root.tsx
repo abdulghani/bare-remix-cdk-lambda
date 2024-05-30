35d596c87b5e9e0050c688b8bb048a3cccdee9c5
Ghani <contact.abdulghani@gmail.com>
2024-05-30 09:46:55 +0700

    chore: cleanup

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,7 @@
 import React from "react";
-import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import { Links, Meta, Scripts } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import stylesheet from "./style.css?url";
-import { Button } from "@/components/ui/button";
-import { CardDemo } from "@/components/custom/card-demo";
-
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-
-import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from "@/components/ui/command";
 import { Dashboard } from "./routes/login/route";
 
 export const links: LinksFunction = () => [
