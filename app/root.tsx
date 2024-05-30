c044a91e618fc5fbd013d31a2b3b0ede6ca66d41
Ghani <contact.abdulghani@gmail.com>
2024-05-30 07:52:17 +0700

    chore: add login screen

--- app/root.tsx
+++ app/root.tsx
@@ -28,6 +28,7 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
+import { Dashboard } from "./routes/login/route";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -36,54 +37,14 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html className="dark">
+    <html className="">
       <head>
         <title>Remix Starter</title>
         <Meta />
         <Links />
       </head>
       <body>
-        <h1 className="text-3xl font-bold">Hello world there!</h1>
-        <img src="/diagram.drawio.svg" />
-
-        <Outlet />
-
-        <Command>
-          <CommandInput placeholder="Type a command or search..." />
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading="Suggestions">
-              <CommandItem>Calendar</CommandItem>
-              <CommandItem>Search Emoji</CommandItem>
-              <CommandItem>Calculator</CommandItem>
-            </CommandGroup>
-            <CommandSeparator />
-            <CommandGroup heading="Settings">
-              <CommandItem>Profile</CommandItem>
-              <CommandItem>Billing</CommandItem>
-              <CommandItem>Settings</CommandItem>
-            </CommandGroup>
-          </CommandList>
-        </Command>
-
-        <CardDemo />
-        <AlertDialog>
-          <AlertDialogTrigger>Open</AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-
+        <Dashboard />
         <Scripts />
       </body>
     </html>
